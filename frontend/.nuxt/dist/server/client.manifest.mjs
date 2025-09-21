f63b41e51fafded3554e8876ceec72979ae57a20
Jarvis <13g7895123@gmail.com>
2025-09-21 17:42:00 +0800

    fix: api url error

--- frontend/.nuxt/dist/server/client.manifest.mjs
+++ frontend/.nuxt/dist/server/client.manifest.mjs
@@ -1,18 +1,1 @@
-export default {
-  "@vite/client": {
-    "prefetch": true,
-    "isEntry": true,
-    "file": "@vite/client",
-    "css": [],
-    "module": true,
-    "resourceType": "script"
-  },
-  "D:/Bonus/0814_web_45000/urban_renewal/frontend/node_modules/nuxt/dist/app/entry.js": {
-    "resourceType": "script",
-    "module": true,
-    "prefetch": true,
-    "preload": true,
-    "isEntry": true,
-    "file": "D:/Bonus/0814_web_45000/urban_renewal/frontend/node_modules/nuxt/dist/app/entry.js"
-  }
-}
+export { default } from "file:///D:/Bonus/0814_web_45000/urban_renewal/frontend/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
